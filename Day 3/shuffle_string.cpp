02abeee9f5f4652cda75333cb7316619e8fcea27
LangerManvi70 <85550069+LangerManvi70@users.noreply.github.com>
2021-07-02 00:31:10 +0530

    Day 3 Questions

---
+++ Day 3/shuffle_string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string res=s;
+        for(int i=0;i<s.length();i++)
+        {
+            res[indices[i]]=s[i];
+        }
+        return res;
+    }
+};
+© 2021 GitHub, Inc.
