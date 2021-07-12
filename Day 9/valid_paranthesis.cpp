2fe4f6ff9ca765d7ac0866181412abd0b1ec9a09
LangerManvi70 <85550069+LangerManvi70@users.noreply.github.com>
2021-07-12 13:10:25 +0530

    Day 9 Questions

---
+++ Day 9/valid_paranthesis.cpp
@@ -0,0 +1,29 @@
+public:
+    bool isValid(string s) {
+        string p="";
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='('||s[i]=='{'||s[i]=='[')
+            {
+                p.push_back(s[i]);
+            }
+            else{
+                char b=p.back();
+                if(b=='('&&s[i]==')'||b=='['&&s[i]==']'||b=='{'&&s[i]=='}')
+                {
+                    p.pop_back();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(p.empty())
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
