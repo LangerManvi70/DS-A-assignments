b6f20a3c211bc7280adadb2b55a79e92173b73db
LangerManvi70 <85550069+LangerManvi70@users.noreply.github.com>
2021-07-13 23:03:59 +0530

    Day 10 Questions

---
+++ Day 10/excel_sheet.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string convertToTitle(int c) {
+        string ans="";
+        while(c)
+        {
+         int r=(c-1)%26;
+            char a='A'+r;
+            c=(c-1)/26;
+        ans.insert(0,1,a);
+        }
+        return ans;
+    }
+};
